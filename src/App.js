edd9b79619668b3113f747c84a40a94628eb335f
Laura Dobie <moving.toyshop@gmail.com>
2020-12-15 15:47:55 +0000

    Fizzbuzz works, gamification a work in progress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useState(1);
+  const [output, setOutput] = useState(0);
+  const [guess, setGuess] = useState(0);
+
+  const increment = (event) => {
+    setNumber(number + 1);
+  }
+
+  const newGuess = (event) => {
+    setGuess(event.target.value);
+  }
+
+  useEffect(()=>{
+      if (number % 3 === 0 && number % 5 === 0){
+        setOutput("fizzbuzz")
+      }
+      else if (number % 3 === 0){
+        setOutput("fizz");
+      }
+      else if (number % 5 === 0){
+        setOutput("buzz");
+      }
+      else {
+        setOutput("Nothing to see here!")
+      }
+
+  }, [number]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Fizzbuzz game</h1>
+      <p>{number}</p>
+      <p>Output: {output}</p>
+      <button onClick={increment}>+</button>
+
+      <input type="text" placeholder="Your guess" value={guess} />
+      <button type="submit" onClick={newGuess}>Check</button>
+    </>
   );
 }
 
